da9c4888005071d29bf703f0073d2783c9a287e2
CappuccinoRose <3163880836@qq.com>
2026-02-18 20:45:57 +0800

    write document to expain

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
+// craco.config.js
 module.exports = {
-    webpack: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    configure: (webpackConfig) => {
+      // 确保 JSON 文件被正确处理
+      webpackConfig.module.rules.push({
+        test: /\.json$/,
+        type: 'json',
+        parser: {
+          parse: JSON.parse
         }
+      });
+      return webpackConfig;
     }
+  }
 }
