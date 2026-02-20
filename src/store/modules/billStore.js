7a4590d86e2bb33ca262c8b7519d07e55c4bbf8f
CappuccinoRose <3163880836@qq.com>
2026-02-20 08:56:50 +0800

    add billstore.js

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -1,36 +1,50 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-//定义store
 const billStore = createSlice({
-    name: 'billStore',
-    //定义store的初始状态
-    initialState: {
-        billList: [],
+  name: 'billStore',
+  initialState: {
+    billList: [],
+  },
+  reducers: {
+    // 1. 同步修改store的方法：设置列表
+    setBillList: (state, action) => {
+      state.billList = action.payload;
     },
-    //定义store的修改方法
-    reducers: {
-        //同步修改store的方法
-        setBillList: (state, action) => {
-            state.billList = action.payload;
-        }
-        },
+    // 2. 同步添加账单的方法
+    addBill: (state, action) => {
+      state.billList.push(action.payload);
+    }
+  },
 })
 
-//解构出ActionCreator函数
-export const {setBillList} = billStore.actions;
+// 3. 解构出 ActionCreator 函数
+export const { setBillList, addBill } = billStore.actions;
 
-//异步修改store的方法
+// 异步获取列表方法
 const getBillList = () => {
-    return async (dispatch) => {
-        //异步获取数据
-        const res = await axios.get('http://localhost:8000/ka')
-        //调用同步修改store的方法
-        dispatch(setBillList(res.data));
-    }
+  return async (dispatch) => {
+    const res = await axios.get('http://localhost:8000/ka')
+    dispatch(setBillList(res.data));
+  }
 }
 
-//导出reducer
+// 4. 异步添加账单方法
+// 接收组件传递的 billData，请求接口后，使用同步方法更新状态
+const postBill = (billData) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.post('http://localhost:8000/ka', billData);
+      // 注意：这里使用 res.data，因为 json-server 返回的是创建后的对象
+      dispatch(addBill(res.data));
+      return res.data; // 返回数据以便组件判断成功
+    } catch (error) {
+      throw error; // 抛出错误以便组件 catch 捕获
+    }
+  }
+}
+
 export default billStore.reducer;
-//导出异步修改store的方法   
-export {getBillList};
+
+// 导出异步方法
+export { getBillList, postBill };
